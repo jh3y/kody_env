59aca839c23288ac33fc8b53fd88c2e5e11ee8fc
jh3y <jh3y@users.noreply.github.com>
2019-07-13 11:03:37 +0100

    feat: set up keybindings for tasks in VSCode

--- kody.tasks/vscode.js
+++ kody.tasks/vscode.js
@@ -16,11 +16,14 @@ const task = {
       const $HOME = shell.exec('echo $HOME', { silent: true }).trim()
       info(log('Symlinking Visual Studio Code settings & snippets'))
       const settings = `${cwd}/vscode/settings.json`
+      const keybindings = `${cwd}/vscode/keybindings.json`
       const snippets = `${cwd}/vscode/snippets/`
       // Where to link settings file to is documented under https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations
       const vsBase = `${$HOME}/Library/Application Support/Code/User`
       info(log(`Linking ${settings} to ${vsBase}/settings.json`))
       shell.ln('-sf', settings, `${vsBase}/settings.json`)
+      info(log(`Linking ${keybindings} to ${vsBase}/keybindings.json`))
+      shell.ln('-sf', keybindings, `${vsBase}/keybindings.json`)
       info(log(`Linking ${snippets} to ${vsBase}/snippets`))
       shell.ln('-sf', snippets, `${vsBase}/snippets`)
       info(log('Visual Studio Code settings linked', '#FFFFFF', '#26a65b'))
